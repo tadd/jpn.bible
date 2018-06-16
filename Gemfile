625e4ea030aa860fd4a12c11d0b8d79fbce0f34b
Tadashi Saito <tad.a.digger@gmail.com>
2018-06-16 16:07:59 +0900

    prepare for development

--- Gemfile
+++ Gemfile
@@ -3,3 +3,8 @@ source 'https://rubygems.org'
 ruby '~> 2.5.1' if ENV['DYNO']
 
 gem 'sinatra'
+
+group :development do
+  gem 'sinatra-contrib'
+  gem 'test-unit'
+end
--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
 require 'sinatra/base'
 
 class JpnBible < Sinatra::Base
+  configure :development do
+    require 'sinatra/reloader'
+    register Sinatra::Reloader
+  end
+
   # renders /public
 end
